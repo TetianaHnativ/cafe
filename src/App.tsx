5ba4df4966d1bd97b1f6afe32f96715b3f9ddc3c
tetiana <tetianahnativ2003@gmail.com>
2024-09-27 17:23:46 +0200

    different scss files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-
 import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
 
 import Header from './pagesElements/Header.tsx';
 import Footer from './pagesElements/Footer.tsx';
 import Content from './pagesElements/Content.tsx';
-
 import store from './store/store.ts';
 
-function App() {
+import './styles/App.css';
+import './styles/HeaderFooter.css';
+import './styles/Main.css';
+import './styles/CartModal.css';
+
+const App: React.FC = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
